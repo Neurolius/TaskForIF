ffdf68233902756e6f00ed9e55ecc8be69ea8e6b
neurolius <ewgensafonow@gmail.com>
2026-02-18 12:11:53 +0800

    Добавлена первая версия необходимой логики

--- Program.cs
+++ Program.cs
@@ -8,6 +8,7 @@
             if (input < 1 || input > 9999) {
                 Console.WriteLine("Значение числа должно быть в рамках: [1;9999]");
             }
+            Console.WriteLine(input/100 + "рублей " + input%100 + "копеек");
         }
     }
 }
