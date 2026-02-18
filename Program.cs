321cb6b4c527742b88196fa0c2178cce48fd05ad
neurolius <ewgensafonow@gmail.com>
2026-02-18 12:45:27 +0800

    Исправлены ошибки и проведена декомпозиция

--- Program.cs
+++ Program.cs
@@ -2,31 +2,36 @@
 {
     internal class Program
     {
+        public static string GetWord(int number, string form1, string form2, string form3)
+        {
+           
+            number = number % 100;
+            int lastDigit = number % 10;
+
+            if (number >= 11 && number <= 19)
+                return form3;
+
+            if (lastDigit == 1)
+                return form1;
+
+            if (lastDigit >= 2 && lastDigit <= 4)
+                return form2;
+
+            return form3;
+        }
+
         static void Main(string[] args)
         {
             int input = int.Parse(Console.ReadLine());
-            string ruble = " рубля ", penny = " копеек ";
+            string ruble, penny;
             if (input < 1 || input > 9999)
             {
                 Console.WriteLine("Значение числа должно быть в рамках: [1;9999]");
             }
             else
             {
-                //Рубли
-                if(input / 100 !=11 && input/100 == 1)
-                {
-                    ruble = " рубль ";
-                }
-                if((input / 100)%10 >=5 && (input / 100)%10 <=9 || (input / 100) % 10 ==0 || ((input / 100) >= 11 && (input / 100) <= 20))
-                {
-                    ruble = " рублей ";
-                }
-
-                //Копейки
-                if (input % 100 != 11 && input % 100 == 1)
-                {
-                    penny = " копейка ";
-                }
+                ruble = GetWord(input / 100, " рубль ", " рубля ", " рублей ");
+                penny = GetWord(input % 100, " копейка ", " копейки ", " копеек ");
                 Console.WriteLine(input / 100 + ruble + input % 100 + penny);
             }
         }
