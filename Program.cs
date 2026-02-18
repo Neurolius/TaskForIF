e0f8bc99303188274932948f3ea72bf9bf0d884e
neurolius <ewgensafonow@gmail.com>
2026-02-18 12:27:39 +0800

    Добавлена черновая версия правильного вывода для копеек

--- Program.cs
+++ Program.cs
@@ -5,13 +5,14 @@
         static void Main(string[] args)
         {
             int input = int.Parse(Console.ReadLine());
-            string ruble = " рубля ", penny;
+            string ruble = " рубля ", penny = " копеек ";
             if (input < 1 || input > 9999)
             {
                 Console.WriteLine("Значение числа должно быть в рамках: [1;9999]");
             }
             else
             {
+                //Рубли
                 if(input / 100 !=11 && input/100 == 1)
                 {
                     ruble = " рубль ";
@@ -20,7 +21,17 @@
                 {
                     ruble = " рублей ";
                 }
-                Console.WriteLine(input / 100 + ruble + input % 100 + "копеек");
+
+                //Копейки
+                if (input % 100 != 11 && input % 100 == 1)
+                {
+                    ruble = " рубль ";
+                }
+                if ((input % 100) % 10 >= 5 && (input % 100) % 10 <= 9 || (input / 100) % 10 == 0 || ((input / 100) >= 11 && (input / 100) <= 20))
+                {
+                    ruble = " рублей ";
+                }
+                Console.WriteLine(input / 100 + ruble + input % 100 + penny);
             }
         }
     }
